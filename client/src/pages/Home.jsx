ee4e71c81202fbd2efd56aed40a0b8e2729e57bf
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-15 21:37:24 +0545

    added home banner and displayed category

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+import DesktopBanner from "../assets/images/banner.jpg";
+import { useSelector } from "react-redux";
+
 function Home() {
-  return <div>Home</div>;
+  const categoryData = useSelector((state) => state?.product.allCategory);
+  console.log("category data", categoryData);
+  return (
+    <section className="container mx-auto  px-4">
+      <div className=" ">
+        <img src={DesktopBanner} alt="banner-img" className="w-full" />
+      </div>
+
+      {/* Display all category */}
+      <div className="grid grid-cols-2 md:gird-cols-4 lg:grid-cols-6 gap-4">
+        {categoryData.map((cat, index) => {
+          return (
+            <div key={cat._id} className=" p-2 ">
+              <img
+                src={cat.image}
+                alt={cat.name}
+                className="rounded-md shadow-md"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
 
 export default Home;
